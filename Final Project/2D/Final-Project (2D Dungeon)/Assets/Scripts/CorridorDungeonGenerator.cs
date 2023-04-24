4a3944299add995dc3ce60c819a47591bba4ce27
Eugene Bryant <eugene.bryant@mymail.champlain.edu>
2023-04-23 22:41:44 -0400

    made the corridor wider

--- Final Project/2D/Final-Project (2D Dungeon)/Assets/Scripts/CorridorDungeonGenerator.cs
+++ Final Project/2D/Final-Project (2D Dungeon)/Assets/Scripts/CorridorDungeonGenerator.cs
@@ -28,7 +28,7 @@ public class CorridorDungeonGenerator : RandomWalkMap
         HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
         HashSet<Vector2Int> potentialRoomPositions = new HashSet<Vector2Int>();
 
-        Create(floorPositions, potentialRoomPositions);
+        List<List<Vector2Int>> corridors = Create(floorPositions, potentialRoomPositions);
 
         HashSet<Vector2Int> roomPositions = CreateRooms(potentialRoomPositions);
 
@@ -37,6 +37,13 @@ public class CorridorDungeonGenerator : RandomWalkMap
         CreateRoomAtDeadEnd(DeadEnd, roomPositions);
 
         floorPositions.UnionWith(roomPositions);
+
+        for(int i = 0; i < corridors.Count; i++)
+        {
+            //corridors[i] = IncreaseCorridorSizeByOne(corridors[i]);
+            corridors[i] = IncreaseCorridorBrush3By3(corridors[i]);
+            floorPositions.UnionWith(corridors[i]);
+        }
 
         tilemapVisualizer.PaintFloorTile(floorPositions);
 
@@ -97,15 +104,19 @@ public class CorridorDungeonGenerator : RandomWalkMap
         return roomPositions;
     }
 
-    private void Create(HashSet<Vector2Int> floorPositions, HashSet<Vector2Int> potentialRoomPositions)
+    private List<List<Vector2Int>> Create(HashSet<Vector2Int> floorPositions, HashSet<Vector2Int> potentialRoomPositions)
     {
         var currentPosition = startPosition;
 
         potentialRoomPositions.Add(currentPosition);
 
+        List<List<Vector2Int>> corridors = new List<List<Vector2Int>>();
+
         for(int i =0; i < corridorCount; i++)
         {
             var corridor = ContentGenerator.RandomCorridor(currentPosition, corridorLength);
+
+            corridors.Add(corridor);
 
             currentPosition = corridor[corridor.Count - 1];
 
@@ -113,5 +124,86 @@ public class CorridorDungeonGenerator : RandomWalkMap
 
             floorPositions.UnionWith(corridor);
         }
+
+        return corridors;
+    }
+
+    public List<Vector2Int> IncreaseCorridorSizeByOne(List<Vector2Int> corridor)
+    {
+        List<Vector2Int> newCorridor = new List<Vector2Int>();
+        Vector2Int previousDirection = Vector2Int.zero;
+
+        for(int i = 1; i < corridor.Count; i++)
+        {
+            Vector2Int directionFromCell = corridor[i] - corridor[i - 1];
+
+            if(previousDirection != Vector2Int.zero && directionFromCell != previousDirection)
+            {
+                //handle corner
+                for(int x = -1; x < 2; x++)
+                {
+                    for(int y = -1; y < 2; y++)
+                    {
+                        newCorridor.Add(corridor[i - 1] + new Vector2Int(x, y));
+                    }
+                }
+
+                previousDirection = directionFromCell;
+            }
+
+            else
+            {
+                //Add a single cell in the direction + 90 degrees
+                Vector2Int newCorridorTileOffset = GetDirection90From(directionFromCell);
+
+                newCorridor.Add(corridor[i - 1]);
+                newCorridor.Add(corridor[i - 1] + newCorridorTileOffset);
+            }
+        }
+
+        return newCorridor;
+    }
+
+    public List<Vector2Int> IncreaseCorridorBrush3By3(List<Vector2Int> corridor)
+    {
+        List<Vector2Int> newCorridor = new List<Vector2Int>();
+
+        for(int i = 1; i < corridor.Count; i++)
+        {
+            for(int x = -1; x < 2; x++)
+            {
+                for(int y = -1; y < 2; y++)
+                {
+                    newCorridor.Add(corridor[i - 1] + new Vector2Int(x, y));
+                }
+            }
+        }
+
+        return newCorridor;
+    }
+
+    private Vector2Int GetDirection90From(Vector2Int direction)
+    {
+        if(direction == Vector2Int.up)
+        {
+            return Vector2Int.right;
+        }
+
+        if(direction == Vector2Int.right)
+        {
+            return Vector2Int.down;
+        }
+
+        if(direction == Vector2Int.down)
+        {
+            return Vector2Int.left;
+        }
+
+        if(direction == Vector2Int.left)
+        {
+            return Vector2Int.up;
+        }
+
+        return Vector2Int.zero;
     }
 }
